8f2880142546061dbd4e245062933a0cecad708a
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-02-08 10:52:05 +0530

    some changes

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://livani-react.envytheme.com"
+    ? "https://hk-gadgets-ecommerce.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
