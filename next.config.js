79906e8bc3eedacedc359c6b42c1f56c03f9ec8d
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-08 18:16:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = {
 	eslint: { ignoreDuringBuilds: true },
 	env: {
 		JWT_SECRET: "zxcvbnmasdfghjklpoiuytrewqasdfghmnbvcxzasdfghjloiuytreqsd",
-		CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/weburetec",
-		CLOUD_NAME: "weburetec",
-		UPLOAD_PRESET: "dtdqwfix",
+    		CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/weburetec/image/upload",
+    		CLOUD_NAME: "weburetec",
+    		UPLOAD_PRESET: "dtdqwfix",
 	},
 	trailingSlash: true,
 };
