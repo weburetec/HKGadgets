3e690dc4e657c008b08cc69b19cab1d9e93055e8
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-02-08 11:41:22 +0530

    baseUrl changed

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://hk-gadgets-ecommerce-5b2gc498e-mayes243.vercel.app"
+    ? "https://hk-gadgets-ecommerce-git-dev-mayes243.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
