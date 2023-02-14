aaa1db5a213e4c3dbb4449581295e35a051490ed
weburetec <58927451+weburetec@users.noreply.github.com>
2023-02-14 12:33:02 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://hk-gadgets.vercel.app"
+    ? "https://www.hkgadgets.com"
     : "http://localhost:3000";
 
 export default baseUrl;
